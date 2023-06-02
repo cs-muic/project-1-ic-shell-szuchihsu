adf8dd77a112e1ccc7972966ffc63e3045c92cc7
Charlie Hsu <schsu@Charlies-MacBook-Pro.local>
2023-06-03 00:20:59 +0700

    Milestone 6

--- icsh.c
+++ icsh.c
@@ -17,7 +17,16 @@ char buffer[MAX_CMD_BUFFER];
 char prevbuffer[MAX_CMD_BUFFER] = "";
 char shell;
 int exitcode = 0;
-pid_t foreground = 0;
+pid_t fg = 0;
+typedef struct {
+    pid_t pid;
+    int jobID;
+    char input[MAX_CMD_BUFFER];
+    int status;
+    int stop;
+} Job;
+int jobcount = 1;
+Job jobs[255];
 
 int isWhitespaceOrNewline(const char* str) {
     while (*str != '\0') {
@@ -31,6 +40,8 @@ int isWhitespaceOrNewline(const char* str) {
 
 
 void external(char* input) {
+    //parsing
+    int bg = 0;
     char copy[MAX_CMD_BUFFER];
     strcpy(copy, input);
     char* args[255];
@@ -40,7 +51,14 @@ void external(char* input) {
         args[i++] = p;
         p = strtok(NULL, " \n");
     }
-    args[i] = p;
+    //background command
+    if (strcmp(args[i-1], "&") == 0) {
+        bg = 1;
+        args[i-1] = NULL;
+    } else {
+        args[i] = p;
+    }
+    //redirect
     int io = -1;
     char* in;
     char* out;
@@ -76,30 +94,37 @@ void external(char* input) {
         dup2(outfile, 1);
         close(outfile);
     }
+    //exec 
     pid_t pid = fork();
     if (pid == 0) {
         execvp(args[0], args);
         exit(1);
     } else {
-        foreground = pid;
-        waitpid(pid, &exitcode, WUNTRACED);
+        if (bg) {
+            int jobID = jobcount++;
+            jobs[jobID].pid = pid;
+            jobs[jobID].jobID = jobID;
+            jobs[jobID].status = 0;
+            jobs[jobID].stop = 0;
+            strcpy(jobs[jobID].input, input); 
+        } else {
+            int jobID = jobcount++;
+            fg = jobID;
+            jobs[jobID].pid = pid;
+            jobs[jobID].jobID = jobID;
+            jobs[jobID].status = 0;
+            jobs[jobID].stop = 0;
+            strcpy(jobs[jobID].input, input);
+            waitpid(pid, &exitcode, WUNTRACED);
+            if (WIFEXITED(exitcode)) {
+                jobs[jobID].status = 1;
+                fg = 0;
+            }
+        }
         dup2(saved_stdout, 1);
         dup2(saved_stdin, 0);
         close(saved_stdout);
         close(saved_stdin);
-        foreground = 0;
-    }
-}
-
-void stophandler() {
-    if (foreground != 0) {
-        kill(foreground, SIGTSTP);
-    }
-}
-
-void inthandler() {
-    if (foreground != 0) {
-        kill(foreground, SIGINT);
     }
 }
 
@@ -133,12 +158,76 @@ void execute(char* input) {
         } else {
             exit(0);
         }
+    } else if (strcmp(command, "jobs") == 0) {
+        for (int i = 1; i < jobcount; i++) {
+            if (jobs[i].stop) {
+                printf("[%d]- Stopped \t%s", jobs[i].jobID, jobs[i].input);
+            } else if (jobs[i].status == 0) {
+                printf("[%d]- Running \t%s", jobs[i].jobID, jobs[i].input);
+            }
+        }
+    } else if (strcmp(command, "fg") == 0) {
+        int jobID = atoi(command + 4);
+        printf("%s", jobs[jobID].input);
+        fg = jobID;
+        jobs[jobID].stop = 0;
+        kill(jobs[jobID].pid, SIGCONT);
+        waitpid(jobs[jobID].pid, &exitcode, 0);
+        jobs[jobID].status = 1;
+    } else if (strcmp(command, "bg") == 0) {
+        int jobID = atoi(command + 4);
+        printf("%s", jobs[jobID].input);
+        jobs[jobID].stop = 0;
+        kill(jobs[jobID].pid, SIGCONT);
     } else {
         strcpy(prevbuffer, buffer);
         external(input);
     }
 }
+void prompt() {
+    shell = 1;
+    while (1) {
+        printf("icsh $ ");
+        fgets(buffer, 255, stdin);
+        execute(buffer);
+    }  
+}
+
+void stophandler() {
+    if (fg != 0) {
+        kill(jobs[fg].pid, SIGTSTP);
+        printf("[%d]+  Stopped \t%s", jobs[fg].jobID, jobs[fg].input);
+        jobs[fg].stop = 1;
+        prompt();
+    }
+}
+
+void inthandler() {
+    if (fg != 0) {
+        kill(jobs[fg].pid, SIGINT);
+        printf("[%d]+  Terminated \t%s", jobs[fg].jobID, jobs[fg].input);
+        jobs[fg].status = 1;
+    }
+}
+
+void childhandler() {
+    int status;
+    pid_t pid;
+    while ((pid = waitpid(-1, &status, WNOHANG | WUNTRACED)) > 0) {
+        for (int i = 1; i < jobcount; i++) {
+            if (jobs[i].pid == pid && i != fg) {
+                if (WIFEXITED(status)) {
+                    jobs[i].status = 1;
+                    printf("[%d]+  Done \t%s", jobs[i].jobID, jobs[i].input);
+                }
+                break;
+            }
+        }
+    }
+}
+
 int main(int argc, char *argv[]) {
+    signal(SIGCHLD, childhandler);
     signal(SIGTSTP, stophandler);
     signal(SIGINT, inthandler);
     if (argc > 1) {
@@ -150,11 +239,6 @@ int main(int argc, char *argv[]) {
             execute(buffer);
         } 
     } else {
-        shell = 1;
-        while (1) {
-            printf("icsh $ ");
-            fgets(buffer, 255, stdin);
-            execute(buffer);
-        }        
+        prompt();      
     }
 }
